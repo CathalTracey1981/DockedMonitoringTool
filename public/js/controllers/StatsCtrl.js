e6267055e26e77e657599393f55d9b53a9716139
cathaltracey <cathaltracey@gmail.com>
2016-03-21 04:36:56 +0000

    Made a start on adding real-time statistics

--- public/js/controllers/StatsCtrl.js
+++ public/js/controllers/StatsCtrl.js
@@ -1,7 +1,21 @@
-angular.module('StatsCtrl', []).controller('statsController', function($scope, $http, $routeParams) {
+angular.module('StatsCtrl', []).controller('statsController', function($scope, $http, $routeParams, $timeout) {
     var id = $routeParams.id;
-    $http.get("/containers/" + id + "/stats?stream=false").success(function (data) {
-        console.log(data);
-        $scope.message = data;
-    });
+    $scope.getStats = function () {
+        $http.get("/containers/" + id + "/stats?stream=false").success(function (data) {
+            console.log(data);
+            $scope.message = data;
+        });
+    };
+
+    // Function to replicate setInterval using $timeout service.
+    $scope.internalFunction = function(){
+        $timeout(function() {
+            $scope.getStats();
+            $scope.internalFunction();
+        }, 3000)
+    };
+
+    // Kick off the interval
+    $scope.internalFunction();
+
 });
