8e77cedb91e47772045254fa97fa10fa43f1fb12
cathaltracey <cathaltracey@gmail.com>
2016-03-23 07:41:09 +0000

    Got Statistics for CPU displaying

--- public/js/controllers/StatsCtrl.js
+++ public/js/controllers/StatsCtrl.js
@@ -2,34 +2,96 @@ angular.module('StatsCtrl', []).controller('statsController', function($scope, $
     var id = $routeParams.id;
 
     $scope.getStats = function () {
-        $http.get("/containers/" + id + "/stats?stream=false").success(function (data1) {
-            console.log(data1);
-            $scope.message = data1;
+        $http.get("/containers/" + id + "/stats?stream=false").success(function (data) {
+            console.log(data);
+            $scope.message = data;
 
             // Network Metrics
-            var tx = data1.network.tx_bytes / 100000;
-            var rx = data1.network.rx_bytes / 100000;
+            var tx = data.network.tx_bytes / 10000;
+            var rx = data.network.rx_bytes / 10000;
+            //var a =  convertToBytes(rx);
 
             // CPU Metrics
-            //var cpuUsage = data1.
+            var cpuUsage = data.precpu_stats.cpu_usage.total_usage / 1000000000;
+            var cpuSystemUsage = data.precpu_stats.system_cpu_usage / 10000000000000;
 
-
-            var canvas = document.getElementById('updating-chart'),
+            // Network Chart (tx, rx)
+            var canvas = document.getElementById('updating-network'),
                 ctx = canvas.getContext('2d'),
                 startingData = {
-                    labels: ['TX_Bytes','RX_Bytes'],
+                    labels: ['RX_Bytes','TX_Bytes'],
                     datasets: [
                         {
                             Label: "GB",
                             fillColor: "rgb(48, 109, 171)",
                             strokeColor: "rgba(220,220,220,0.8)",
-                            highlightFill: "rgb(32, 43, 120)",
+                            highlightFill: "rgb(48, 109, 171)",
                             highlightStroke: "rgba(220,220,220,1)",
-                            data: [tx, rx]
+                            interactivityEnabled: false,
+                            data: [rx, tx]
                         }
                     ]
                 };
 
+            var myLiveChart = new Chart(ctx).Bar(startingData,
+                {
+                    animationSteps: 1,
+                    scaleSteps : 3,
+                    scaleStepWidth : 10,
+                    scaleStartValue : 0
+                    //interactivityEnabled: false,
+
+                });
+
+
+            // Container CPU Usage (%)
+            var canvas = document.getElementById('updating-cpu'),
+                ctx = canvas.getContext('2d'),
+                startingData = {
+                    labels: ['CPU'],
+                    datasets: [
+                        {
+                            Label: "GB",
+                            fillColor: "rgb(21, 85, 21)",
+                            strokeColor: "rgba(220,220,220,0.8)",
+                            highlightFill: "rgb(21, 85, 21)",
+                            highlightStroke: "rgba(220,220,220,1)",
+                            interactivityEnabled: false,
+                            data: [cpuUsage]
+                        }
+                    ]
+                };
+
+            // Reduce the animation steps for demo clarity.
+            var myLiveChart = new Chart(ctx).Bar(startingData,
+                {
+
+                    animationSteps: 1,
+                    scaleSteps : 3,
+                    scaleStepWidth : 10,
+                    scaleStartValue : 0
+
+
+
+                });
+
+            // System CPU Usage
+            var canvas = document.getElementById('updating-system-cpu'),
+                ctx = canvas.getContext('2d'),
+                startingData = {
+                    labels: ['System CPU'],
+                    datasets: [
+                        {
+                            Label: "GB",
+                            fillColor: "rgb(21, 85, 21)",
+                            strokeColor: "rgba(220,220,220,0.8)",
+                            highlightFill: "rgb(21, 85, 21)",
+                            highlightStroke: "rgba(220,220,220,1)",
+                            interactivityEnabled: false,
+                            data: [ cpuSystemUsage]
+                        }
+                    ]
+                };
 
             // Reduce the animation steps for demo clarity.
             var myLiveChart = new Chart(ctx).Bar(startingData,
@@ -39,33 +101,10 @@ angular.module('StatsCtrl', []).controller('statsController', function($scope, $
                     scaleSteps : 3,
                     scaleStepWidth : 10,
                     scaleStartValue : 0,
-                    tooltipTitleFontFamily: " Arial",
-                    // Number - Tooltip title font size in pixels
-                    tooltipTitleFontSize: 14,
 
-                    // String - Tooltip title font weight style
-                    tooltipTitleFontStyle: "bold",
 
-                    // String - Tooltip title font colour
-                    tooltipTitleFontColor: "#fff",
-
-                    // Number - pixel width of padding around tooltip text
-                    tooltipYPadding: 6,
-
-                    // Number - pixel width of padding around tooltip text
-                    tooltipXPadding: 6,
-
-                    // Number - Size of the caret on the tooltip
-                    tooltipCaretSize: 8,
-
-                    // Number - Pixel radius of the tooltip border
-                    tooltipCornerRadius: 6,
-
-                    // Number - Pixel offset from point x to tooltip edge
-                    tooltipXOffset: 10
 
                 });
-
         });
 
     };
@@ -76,7 +115,7 @@ angular.module('StatsCtrl', []).controller('statsController', function($scope, $
             $scope.getStats();
             $scope.intervalFunction();
 
-        }, 500)
+        }, 1000)
     };
 
     // Kick off the interval
