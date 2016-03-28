258626c50aa70d4faf2cb5845f0c42a2e9ac9097
cathaltracey <cathaltracey@gmail.com>
2016-03-28 11:47:43 +0100

    comment

--- test/routesTest.js
+++ test/routesTest.js
@@ -5,7 +5,7 @@ var server = require('../server');
 var app = server;
 describe('loading express', function () {
 
-
+// comment
     it('responds to /', function testSlash(done) {
         request(express)
             .get('/info')
